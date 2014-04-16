f1fa5c2b5322b3e711324919fc1207d450fdd4df
Carson Wright <carsonnwright@gmail.com>
2014-04-15 23:24:38 -0400

    initial commit

---
+++ traitify-py2.py
@@ -0,0 +1,25 @@
+import httplib, urllib, base64, json
+class Assessment:
+	def __init__(self, attributes):
+		self.id = attributes["id"]
+		self.user_id = attributes["user_id"]
+		self.deck_id = attributes["deck_id"]
+		self.completed_at = attributes["completed_at"]
+		self.created_at = attributes["created_at"]
+
+class Traitify:
+	def __init__(self, configurations = {"url":"", "secret_key":"", "public_key":"", "deck_id":""}):
+		self.url = configurations["url"]
+		self.secret_key = configurations["secret_key"]
+		self.public_key = configurations["public_key"]
+		self.deck_id = configurations["deck_id"]
+
+	def create_assessment(self):
+		headers = {"Accept":"application/json", "Content-Type":"application/json", "Authorization": "Basic "+self.private_key+":x"}
+		conn = httplib.HTTPConnection(self.url)
+		conn.request("POST", "/"+self.version+"/assessments", '{"deck_id":"'+self.deck_id+'"}', headers)
+		response = conn.getresponse()
+		data = response.read()
+		conn.close()
+		json_data = json.loads(data)
+		return Assessment(json_data)
