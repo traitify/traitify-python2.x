a9c39fe1f4be929b0f0f579ec3cfa0bfa49f6166
Carson Wright <carsonnwright@gmail.com>
2014-04-16 15:45:11 -0400

    changed url to host

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
-traitify-python2.x
+Traitify  Python 2.x
 ==================
-
-
 
 ```
 traitify = Traitify()
-traitify.url = "api.traitify.com"
+traitify.host = "api.traitify.com"
 traitify.deck_id = "your deck id"
 traitify.public_key = "your public key"
 traitify.private_key = "your private key"
 traitify.version = "v1"
-print traitify.create_assessment().id
+print(traitify.create_assessment().id)
 ```
--- traitify-py2.py
+++ traitify-py2.py
@@ -8,15 +8,15 @@ class Assessment:
 		self.created_at = attributes["created_at"]
 
 class Traitify:
-	def __init__(self, configurations = {"url":"", "secret_key":"", "public_key":"", "deck_id":""}):
-		self.url = configurations["url"]
+	def __init__(self, configurations = {"host":"", "secret_key":"", "public_key":"", "deck_id":""}):
+		self.host = configurations["host"]
 		self.secret_key = configurations["secret_key"]
 		self.public_key = configurations["public_key"]
 		self.deck_id = configurations["deck_id"]
 
 	def create_assessment(self):
 		headers = {"Accept":"application/json", "Content-Type":"application/json", "Authorization": "Basic "+self.private_key+":x"}
-		conn = httplib.HTTPConnection(self.url)
+		conn = httplib.HTTPConnection(self.host)
 		conn.request("POST", "/"+self.version+"/assessments", '{"deck_id":"'+self.deck_id+'"}', headers)
 		response = conn.getresponse()
 		data = response.read()
