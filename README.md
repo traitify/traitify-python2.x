23f2fd037692470a80088650aa8c3d48cd37043b
Tom Prats <tprats108@gmail.com>
2014-07-02 17:41:07 -0400

    Added all the endpoints and a walkthrough in the readme

--- README.md
+++ README.md
@@ -2,11 +2,47 @@ Traitify  Python 2.x
 ==================
 
 ```
-traitify = Traitify()
-traitify.host = "api.traitify.com"
-traitify.deck_id = "your deck id"
-traitify.public_key = "your public key"
-traitify.private_key = "your private key"
-traitify.version = "v1"
-print(traitify.create_assessment().id)
+# Import traitify
+from traitify import Traitify
+
+# Initialize and Authenticate
+secret_key = "Your Secret Key"
+traitify = Traitify(secret_key)
+
+# Get the decks
+decks = traitify.get_decks()
+
+# Set deck key
+traitify.deck_key = decks[0].key
+
+# Create an assessment
+assessment = traitify.create_assessment()
+
+# Get an assessment
+assessment = traitify.get_assessment(assessment.id)
+
+# Get an assessment's slides
+slides = traitify.get_slides(assessment.id)
+
+# Upate a slide
+slide = slides[0]
+slide.response = True
+slide.time_taken = 200
+slide = traitify.update_slide(assessment.id, slide)
+
+# Bulk update slides
+for slide in slides:
+  slide.response = True
+  slide.time_taken = 200
+slides = traitify.update_slides(assessment.id, slides)
+
+# Get an assessment's results (personality types)
+personality_types = traitify.get_personality_types(assessment.id)
+
+# Get an assessment's results (personality type traits)
+personality_type = personality_types["personality_types"][0]["personality_type"]
+personality_traits = traitify.get_personality_type_traits(assessment.id, personality_type.id)
+
+# Get an assessment's results (personality traits)
+personality_traits = traitify.get_personality_traits(assessment.id)
 ```
---
+++ traitify.py
@@ -0,0 +1,198 @@
+import httplib, urllib, base64, json
+class TraitifyException(Exception):
+  pass
+
+def check_errors(response):
+  if type(response) is dict and response.get("errors") is not None:
+    raise TraitifyException(", ".join(response["errors"]))
+
+class Deck:
+  def __init__(self, attributes):
+    self.id = attributes.get("id")
+    self.key = attributes.get("key")
+    self.name = attributes.get("name")
+    self.description = attributes.get("description")
+    self.attributes = attributes
+
+class Assessment:
+  def __init__(self, attributes):
+    self.id = attributes.get("id")
+    self.deck_id = attributes.get("deck_id")
+    self.deck_key = attributes.get("deck_key")
+    self.completed_at = attributes.get("completed_at")
+    self.created_at = attributes.get("created_at")
+    self.attributes = attributes
+
+class Slide:
+  def __init__(self, attributes):
+    self.id = attributes.get("id")
+    self.position = attributes.get("position")
+    self.image_desktop = attributes.get("image_desktop")
+    self.image_desktop_retina = attributes.get("image_desktop_retina")
+    self.image_phone_landscape = attributes.get("image_phone_landscape")
+    self.image_phone_portrait = attributes.get("image_phone_portrait")
+    self.response = attributes.get("response")
+    self.time_taken = attributes.get("time_taken")
+    self.completed_at = attributes.get("completed_at")
+    self.created_at = attributes.get("created_at")
+    self.attributes = attributes
+
+class PersonalityBlend:
+  def __init__(self, attributes):
+    self.id = attributes.get("id")
+    self.personality_type_1 = PersonalityType(attributes.get("personality_type_1"))
+    self.personality_type_2 = PersonalityType(attributes.get("personality_type_2"))
+    self.name = attributes.get("name")
+    self.description = attributes.get("description")
+    self.compliments = attributes.get("compliments")
+    self.conflicts = attributes.get("conflicts")
+    self.compatible_work_environment_1 = attributes.get("compatible_work_environment_1")
+    self.compatible_work_environment_2 = attributes.get("compatible_work_environment_2")
+    self.compatible_work_environment_3 = attributes.get("compatible_work_environment_3")
+    self.compatible_work_environment_4 = attributes.get("compatible_work_environment_4")
+    self.attributes = attributes
+
+class PersonalityType:
+  def __init__(self, attributes):
+    self.id = attributes.get("id")
+    self.name = attributes.get("name")
+    self.description = attributes.get("description")
+    self.badge = Badge(attributes.get("badge"))
+    self.compatible_personality_type_id = attributes.get("compatible_personality_type_id")
+    famous_people = []
+    for famous_person in attributes.get("famous_people"):
+      famous_people.append(FamousPerson(famous_person))
+    self.famous_people = famous_people
+    self.attributes = attributes
+
+class Badge:
+  def __init__(self, attributes):
+    self.image_small = attributes.get("image_small")
+    self.image_medium = attributes.get("image_medium")
+    self.image_large = attributes.get("image_large")
+    self.font_color = attributes.get("font_color")
+    self.color_1 = attributes.get("color_1")
+    self.color_2 = attributes.get("color_2")
+    self.color_3 = attributes.get("color_3")
+    self.attributes = attributes
+
+class FamousPerson:
+  def __init__(self, attributes):
+    self.id = attributes.get("id")
+    self.name = attributes.get("name")
+    self.description = attributes.get("description")
+    self.picture = attributes.get("picture")
+    self.attributes = attributes
+
+class PersonalityTrait:
+  def __init__(self, attributes):
+    self.name = attributes.get("name")
+    self.definition = attributes.get("definition")
+    self.description = attributes.get("description")
+    self.attributes = attributes
+
+class Traitify:
+  def __init__(self, secret_key=None, deck_key=None, host="api-sandbox.traitify.com", version="v1"):
+    self.host = host
+    self.secret_key = secret_key
+    self.deck_key = deck_key
+    self.version = version
+
+  def headers(self):
+    return { "Accept":"application/json", "Content-Type":"application/json", "Authorization":"Basic " + self.secret_key + ":x" }
+
+  def get(self, path):
+    conn = httplib.HTTPConnection(self.host)
+    conn.request("GET", "/" + self.version + path, None, self.headers())
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+    response = json.loads(data)
+    check_errors(response)
+    return response
+
+  def post(self, path, arguments):
+    conn = httplib.HTTPConnection(self.host)
+    conn.request("POST", "/" + self.version + path, arguments, self.headers())
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+    response = json.loads(data)
+    check_errors(response)
+    return response
+
+  def put(self, path, arguments):
+    conn = httplib.HTTPConnection(self.host)
+    conn.request("PUT", "/" + self.version + path, arguments, self.headers())
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+    response = json.loads(data)
+    check_errors(response)
+    return response
+
+  def get_decks(self):
+    data = self.get("/decks")
+    decks = []
+    for deck in data:
+      decks.append(Deck(deck))
+    return decks
+
+  def create_assessment(self, deck_key=None):
+    if deck_key is None: deck_key = self.deck_key
+    data = self.post("/assessments", json.dumps({"deck_key": deck_key}))
+    return Assessment(data)
+
+  def get_assessment(self, assessment_id):
+    data = self.get("/assessments/" + assessment_id)
+    return Assessment(data)
+
+  def get_slides(self, assessment_id):
+    data = self.get("/assessments/" + assessment_id + "/slides")
+    slides = []
+    for slide in data:
+      slides.append(Slide(slide))
+    return slides
+
+  def update_slide(self, assessment_id, slide):
+    slim_slide = {"response": slide.response, "time_taken": slide.time_taken}
+    data = self.put("/assessments/" + assessment_id + "/slides/" + slide.id, json.dumps(slim_slide))
+    return Slide(data)
+
+  def update_slides(self, assessment_id, slides):
+    slim_slides = []
+    for slide in slides:
+      slim_slides.append({"id": slide.id, "response": slide.response, "time_taken": slide.time_taken})
+    data = self.put("/assessments/" + assessment_id + "/slides", json.dumps(slim_slides))
+    slides = []
+    for slide in data:
+      slides.append(Slide(slide))
+    return slides
+
+  def get_personality_types(self, assessment_id):
+    data = self.get("/assessments/" + assessment_id + "/personality_types")
+    personality_types = []
+    for personality_type in data.get("personality_types"):
+      personality_types.append({
+        "personality_type": PersonalityType(personality_type.get("personality_type")),
+        "score": personality_type.get("score") })
+    return { "personality_blend": PersonalityBlend(data.get("personality_blend")), "personality_types": personality_types }
+
+  def get_personality_type_traits(self, assessment_id, personality_type_id):
+    data = self.get("/assessments/" + assessment_id + "/personality_types/" + personality_type_id + "/personality_traits")
+    personality_traits = []
+    for personality_trait in data:
+      personality_traits.append({
+        "personality_trait": PersonalityTrait(personality_trait.get("personality_trait")),
+        "score": personality_trait.get("score") })
+    return personality_traits
+
+  def get_personality_traits(self, assessment_id):
+    data = self.get("/assessments/" + assessment_id + "/personality_traits")
+    personality_traits = []
+    for personality_trait in data:
+      personality_traits.append({
+        "left_personality_trait": PersonalityTrait(personality_trait.get("left_personality_trait")),
+        "right_personality_trait": PersonalityTrait(personality_trait.get("right_personality_trait")),
+        "score": personality_trait.get("score") })
+    return personality_traits
